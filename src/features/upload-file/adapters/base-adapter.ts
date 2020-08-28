f07d29a006f1c4985c5fbb386c7161af2c012fb6
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-08-28 16:01:28 +0200

    chore: implement BaseAdapter as a abstract class

--- src/features/upload-file/adapters/base-adapter.ts
+++ src/features/upload-file/adapters/base-adapter.ts
@@ -1,3 +1,15 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 export default class BaseAdapter {
+  public async upload(tmpFile: Buffer, key: string): Promise<any> {
+    throw new Error('You have to implement upload method')
+  }
 
+  public async delete(key: string, bucket: string): Promise<any> {
+    throw new Error('You have to implement delete method')
+  }
+
+  public async path(key: string, bucket: string): Promise<any> {
+    throw new Error('You have to implement path method')
+  }
 }
