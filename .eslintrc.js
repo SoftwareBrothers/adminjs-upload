dc20923678119ff443e826ef1189a07af9d80a11
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-08-29 18:04:54 +0200

    style: fix linter issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   ignorePatterns: [
     '*/build/**/*',
     '*.json',
+    '*.txt',
     'yarn.lock',
     '*.yaml',
   ],
@@ -48,7 +49,7 @@ module.exports = {
       'import/no-extraneous-dependencies': 'off',
     },
   }, {
-    files: ['./src/**/*.spec.ts'],
+    files: ['./src/**/*.spec.ts', '**/*/spec/**/*.ts'],
     rules: {
       'no-unused-expressions': 'off',
       'import/no-extraneous-dependencies': 'off',
