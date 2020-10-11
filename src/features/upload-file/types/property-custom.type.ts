43f6ad6b4623751bb6703c9a1d82b381b36a3e04
Wojciech <wojciech.g.krysiak@gmail.com>
2020-10-11 06:03:59 +0200

    fix: gitignore settings for types

---
+++ src/features/upload-file/types/property-custom.type.ts
@@ -0,0 +1,22 @@
+import { MimeType } from './mime-types.type'
+
+/**
+ * Custom ({@link PropertyOptions#custom}) properties passed down to the component.
+ * @private
+ */
+type PropertyCustom = {
+  filePathProperty: string,
+  fileProperty: string,
+  filesToDeleteProperty: string,
+  keyProperty: string,
+  fileNameProperty?: string,
+  mimeTypeProperty?: string,
+  bucketProperty?: string,
+  defaultBucket: string,
+  mimeTypes?: Array<MimeType>,
+  maxSize?: number,
+  provider: string,
+  multiple: boolean,
+}
+
+export default PropertyCustom
