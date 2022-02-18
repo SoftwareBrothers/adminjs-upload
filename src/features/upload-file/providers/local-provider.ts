bb4e41c11157626d180ccee837f9a67f7ae2d1df
Arian Sobczak <arian.sobczak@rst.com.pl>
2022-02-18 11:06:30 +0100

    chore: fixed linting

--- src/features/upload-file/providers/local-provider.ts
+++ src/features/upload-file/providers/local-provider.ts
@@ -27,21 +27,24 @@ export class LocalProvider extends BaseProvider {
 
   public async upload(file: UploadedFile, key: string): Promise<any> {
     const filePath = process.platform === 'win32'
-        ? this.path(key) : this.path(key).slice(1); // adjusting file path according to OS
+      ? this.path(key) : this.path(key).slice(1) // adjusting file path according to OS
 
-    await fs.promises.mkdir(path.dirname(filePath), { recursive: true });
-    await fs.promises.rename(file.path, filePath);
+    await fs.promises.mkdir(path.dirname(filePath), { recursive: true })
+    await fs.promises.rename(file.path, filePath)
   }
 
   public async delete(key: string, bucket: string): Promise<any> {
-    await fs.promises.unlink(process.platform === 'win32'
-        ? this.path(key, bucket) : this.path(key, bucket).slice(1)); // adjusting file path according to OS
+    await fs.promises.unlink(
+      process.platform === 'win32'
+        ? this.path(key, bucket)
+        : this.path(key, bucket).slice(1),
+    ) // adjusting file path according to OS
   }
 
   // eslint-disable-next-line class-methods-use-this
   public path(key: string, bucket?: string): string {
     // Windows doesn't requires the '/' in path, while UNIX system does
     return process.platform === 'win32' ? `${path.join(bucket || this.bucket, key)}`
-        : `/${path.join(bucket || this.bucket, key)}`;
+      : `/${path.join(bucket || this.bucket, key)}`
   }
 }
