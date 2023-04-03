1680a345c1afb71b079fd78dd05d04bb74ee42a8
Jarosław Wasiak <jaroslaw.wasiak@rst.com.pl>
2023-04-03 08:02:52 +0000

    chore: chore: changed __dirname to dirname

--- src/features/upload-file/utils/bundle-component.ts
+++ src/features/upload-file/utils/bundle-component.ts
@@ -1,16 +1,16 @@
-import path from 'path';
-import * as url from 'url';
+import path from 'path'
+import * as url from 'url'
 
-import type { ComponentLoader } from 'adminjs';
+import type { ComponentLoader } from 'adminjs'
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const bundleComponent = (
   loader: ComponentLoader,
   componentName: string,
 ) => {
-  const componentPath = path.join(__dirname, `../components/${componentName}`)
+  const componentPath = path.join(dirname, `../components/${componentName}`)
   return loader.add(componentName, componentPath)
 }
 
