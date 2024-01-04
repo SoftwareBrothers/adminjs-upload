5ed9f5bc220af5f3d65f289eddfe2ca4f473a67c
Thiago Silva <thiago.h.silva.98@gmail.com>
2024-01-04 15:27:01 -0300

    fix: use translateProperty in UploadShowComponent

--- src/features/upload-file/components/UploadShowComponent.tsx
+++ src/features/upload-file/components/UploadShowComponent.tsx
@@ -1,15 +1,16 @@
 import { FormGroup, Label } from '@adminjs/design-system'
-import { ShowPropertyProps } from 'adminjs'
+import { ShowPropertyProps, useTranslation } from 'adminjs'
 import React, { FC } from 'react'
 
 import File from './file.js'
 
 const Show: FC<ShowPropertyProps> = (props) => {
   const { property } = props
+  const { translateProperty } = useTranslation()
 
   return (
     <FormGroup>
-      <Label>{property.label}</Label>
+      <Label>{translateProperty(property.label, property.resourceId)}</Label>
       <File width="100%" {...props} />
     </FormGroup>
   )
